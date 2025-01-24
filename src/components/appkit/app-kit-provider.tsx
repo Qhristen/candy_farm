b7d2abd5d1d7be0b4999c32250e50a656f1b6b27
qhristen <odeyxtian@gmail.com>
2025-01-24 13:54:02 +0100

    fix: update CandyFarm URL in app-kit-provider metadata

--- src/components/appkit/app-kit-provider.tsx
+++ src/components/appkit/app-kit-provider.tsx
@@ -25,7 +25,7 @@ if (!projectId) {
 const metadata = {
   name: "CandyFarm",
   description: "Candy farm wallet connect",
-  url: "https://candyfarm.blaqrio.fun/", // origin must match your domain & subdomain
+  url: "https://blaqrio.fun",
   icons: ["https://assets.reown.com/reown-profile-pic.png"],
 };
 
